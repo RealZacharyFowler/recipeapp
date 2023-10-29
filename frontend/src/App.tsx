2d697f64b3f05620ac7abc7df636c6918b55d759
Zachary Fowler <ZacharySamuel.Fowler@gmail.com>
2023-10-28 22:12:33 -0700

    added in return data
    Signed-off-by: Zachary Fowler <ZacharySamuel.Fowler@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,37 @@
 import { useState } from "react";
 import "./App.css";
+import { searchRecipes } from "./API";
 
 // src/App.tsx
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [recipes, setRecipes] = useState([]);
-  
-  return <div>Hello from Recipe App</div>;
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const { results } = await searchRecipes(searchTerm, 1);
+      setRecipes(results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSearchSubmit}>
+        <button type="submit">Submit</button>
+      </form>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          Recipe Image Location: {recipe.image}
+          <br />
+          Recipe Title: {recipe.title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
