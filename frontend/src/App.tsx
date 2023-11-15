bdd85eb06ca86e89a86e39d7b02704cf61f86183
Zachary Fowler <ZacharySamuel.Fowler@gmail.com>
2023-11-14 20:21:39 -0800

    removed unnecessary import
    Signed-off-by: Zachary Fowler <ZacharySamuel.Fowler@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./App.css";
-import { searchRecipes } from "./API";
 import { Recipe } from "./types";
 import RecipeCard from "./components/RecipeCard";
 
