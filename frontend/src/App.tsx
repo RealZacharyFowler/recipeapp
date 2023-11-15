633ad7057e6a5e5222d27c39ec0ea74afec27eb2
Zachary Fowler <ZacharySamuel.Fowler@gmail.com>
2023-11-14 20:25:07 -0800

    fixed imports and reorganizing file structure
    Signed-off-by: Zachary Fowler <ZacharySamuel.Fowler@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import "./App.css";
 import { Recipe } from "./types";
+import * as api from "./api";
 import RecipeCard from "./components/RecipeCard";
 
 // src/App.tsx
